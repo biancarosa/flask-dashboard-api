4c364b233bc4a62cec2c3c6b64f2a23bc9d86586
Bianca Rosa <me@biancarosa.com.br>
2018-03-30 14:23:42 -0300

    Move imports up

--- dashboard_api/server.py
+++ dashboard_api/server.py
@@ -8,11 +8,11 @@ from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
+from dashboard_api.resources.user import Users
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
-
-from dashboard_api.resources.user import Users
 
 api = Api(app)
 api.add_resource(Users, '/users')
